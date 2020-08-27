256ae7d524961f7b6f66ffc967042710104a89bb
Devin <ZHAODE4@OOCL.com>
2020-08-27 14:30:15 +0800

    [Refactoring][devin] test: add test 'statement case 2 test. play type is tragedy, play audience is 29', 'statement case 3 test. play type is comedy, play audience is 21','statement case 4 test. play type is comedy, play audience is 19'

--- exercise/test/statementTest.js
+++ exercise/test/statementTest.js
@@ -34,3 +34,105 @@ test('statement case 1 test. play type is tragedy, play audience is 40', t => {
       'Amount owed is $500.00\n' +
       'You earned 10 credits \n');
 });
+
+test('statement case 2 test. play type is tragedy, play audience is 29', t => {
+    //given
+    const invoice = {
+        'customer': 'BigCo',
+        'performances': [
+            {
+                'playID': 'othello',
+                'audience': 29,
+            }
+        ],
+    };
+    const plays = {
+        'hamlet': {
+            'name': 'Hamlet',
+            'type': 'tragedy',
+        },
+        'as-like': {
+            'name': 'As You Like It',
+            'type': 'comedy',
+        },
+        'othello': {
+            'name': 'Othello',
+            'type': 'tragedy',
+        },
+    };
+    //when
+    const result = statement(invoice, plays);
+    //then
+    t.is(result, 'Statement for BigCo\n' +
+        ' Othello: $400.00 (29 seats)\n' +
+        'Amount owed is $400.00\n' +
+        'You earned 0 credits \n');
+});
+
+test('statement case 3 test. play type is comedy, play audience is 21', t => {
+    //given
+    const invoice = {
+        'customer': 'BigCo',
+        'performances': [
+            {
+                'playID': 'as-like',
+                'audience': 21,
+            }
+        ],
+    };
+    const plays = {
+        'hamlet': {
+            'name': 'Hamlet',
+            'type': 'tragedy',
+        },
+        'as-like': {
+            'name': 'As You Like It',
+            'type': 'comedy',
+        },
+        'othello': {
+            'name': 'Othello',
+            'type': 'tragedy',
+        },
+    };
+    //when
+    const result = statement(invoice, plays);
+    //then
+    t.is(result, 'Statement for BigCo\n' +
+        ' As You Like It: $468.00 (21 seats)\n' +
+        'Amount owed is $468.00\n' +
+        'You earned 4 credits \n');
+});
+
+test('statement case 4 test. play type is comedy, play audience is 19', t => {
+    //given
+    const invoice = {
+        'customer': 'BigCo',
+        'performances': [
+            {
+                'playID': 'as-like',
+                'audience': 19,
+            }
+        ],
+    };
+    const plays = {
+        'hamlet': {
+            'name': 'Hamlet',
+            'type': 'tragedy',
+        },
+        'as-like': {
+            'name': 'As You Like It',
+            'type': 'comedy',
+        },
+        'othello': {
+            'name': 'Othello',
+            'type': 'tragedy',
+        },
+    };
+    //when
+    const result = statement(invoice, plays);
+    //then
+    t.is(result, 'Statement for BigCo\n' +
+        ' As You Like It: $357.00 (19 seats)\n' +
+        'Amount owed is $357.00\n' +
+        'You earned 3 credits \n');
+});

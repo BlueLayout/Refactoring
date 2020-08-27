2c70977497848ff8bc52d4f3520e53d8f2fe9420
Devin <ZHAODE4@OOCL.com>
2020-08-27 15:42:44 +0800

    [Refactoring][devin] test: add test statement case 6 test. Print xml result when hamlet 55, as-like 35 and othello 40

--- exercise/test/statementTest.js
+++ exercise/test/statementTest.js
@@ -173,3 +173,50 @@ test('statement case 5 test. play type is unknown type, play audience is 19', t 
     //then
     t.is(result.message,"unknown type: tragedys")
 });
+
+test('statement case 6 test. Print xml result when hamlet 55, as-like 35 and othello 40', t => {
+    //given
+    const invoice = {
+        'customer': 'BigCo',
+        'performances': [
+            {
+                'playID': 'hamlet',
+                'audience': 55,
+            },
+            {
+                'playID': 'as-like',
+                'audience': 35,
+            },
+            {
+                'playID': 'othello',
+                'audience': 40,
+            }
+        ],
+    };
+    const plays = {
+        'hamlet': {
+            'name': 'Hamlet',
+            'type': 'tragedy',
+        },
+        'as-like': {
+            'name': 'As You Like It',
+            'type': 'comedy',
+        },
+        'othello': {
+            'name': 'Othello',
+            'type': 'tragedy',
+        }
+    };
+    //when
+    const result = statement(invoice, plays);
+    //then
+    t.is(result, '<h1>Statement for BigCo</h1>\n' +
+        '<table>\n' +
+        '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+        ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+        ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+        ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+        '</table>\n' +
+        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+        '<p>You earned <em>47</em> credits</p>\n');
+});

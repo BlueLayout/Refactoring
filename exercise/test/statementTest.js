9329c84c1a02637ee17c83eb2e569843febc85cc
Devin <ZHAODE4@OOCL.com>
2020-08-27 14:24:19 +0800

    [Refactoring][devin] test: add test 'statement case 1 test. play type is tragedy, play audience is 40'

--- exercise/test/statementTest.js
+++ exercise/test/statementTest.js
@@ -1,29 +1,15 @@
 const test = require('ava');
 const {statement} = require('../src/statement');
 
-test('Sample test', t => {
-  t.true(true);
-  t.is(1, 1);
-  t.deepEqual({a: 1}, {a: 1});
-});
-
-test('statement test', t => {
+test('statement case 1 test. play type is tragedy, play audience is 40', t => {
   //given
   const invoice = {
     'customer': 'BigCo',
     'performances': [
       {
-        'playID': 'hamlet',
-        'audience': 55,
-      },
-      {
-        'playID': 'as-like',
-        'audience': 35,
-      },
-      {
         'playID': 'othello',
         'audience': 40,
-      },
+      }
     ],
   };
   const plays = {
@@ -44,9 +30,7 @@ test('statement test', t => {
   const result = statement(invoice, plays);
   //then
   t.is(result, 'Statement for BigCo\n' +
-      ' Hamlet: $650.00 (55 seats)\n' +
-      ' As You Like It: $580.00 (35 seats)\n' +
       ' Othello: $500.00 (40 seats)\n' +
-      'Amount owed is $1,730.00\n' +
-      'You earned 47 credits \n');
+      'Amount owed is $500.00\n' +
+      'You earned 10 credits \n');
 });
